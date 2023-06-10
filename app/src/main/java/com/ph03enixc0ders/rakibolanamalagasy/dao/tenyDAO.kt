0daae18a23ec406b85a767ab3320f13832390a9c
Tafita29 <rohanrc101@gmail.com>
2023-06-10 23:20:00 +0300

    feat: dao

--- app/src/main/java/com/ph03enixc0ders/rakibolanamalagasy/dao/tenyDAO.kt
+++ app/src/main/java/com/ph03enixc0ders/rakibolanamalagasy/dao/tenyDAO.kt
@@ -1,4 +1,21 @@
 package com.ph03enixc0ders.rakibolanamalagasy.dao
 
-class tenyDAO {
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.ph03enixc0ders.rakibolanamalagasy.entity.teny
+
+@Dao
+interface tenyDAO {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertTeny(teny:teny);
+
+    @Query("Select * from Rakibolana")
+    fun getListTeny():LiveData<List<teny>>
+
+    @Query("SELECT * from Rakibolana where word like %:keyword%")
+    fun getListByWord(words:String):LiveData<List<teny>>;
 }
