1af922d372a187fafb4488f8144abe32147bfd0e
Tafita29 <rohanrc101@gmail.com>
2023-06-10 23:52:30 +0300

    feat: initialize the databinding

--- app/src/main/java/com/ph03enixc0ders/rakibolanamalagasy/views/MainActivity.kt
+++ app/src/main/java/com/ph03enixc0ders/rakibolanamalagasy/views/MainActivity.kt
@@ -3,11 +3,19 @@ package com.ph03enixc0ders.rakibolanamalagasy.views
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.ph03enixc0ders.rakibolanamalagasy.R
+import com.ph03enixc0ders.rakibolanamalagasy.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var _binding:ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding=ActivityMainBinding.inflate(layoutInflater)
+        val view=_binding.root
+        setContentView(view)
+
+        //INITVIEW
         initView()
     }
 
@@ -18,6 +26,9 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun initView(){
+        //HIDE ACTION BAR
         this.supportActionBar?.hide();
+
+        //
     }
 }
