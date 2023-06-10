5cf9a156f145107e5ed1c16c91099b64298b0e4b
Tafita29 <rohanrc101@gmail.com>
2023-06-10 23:09:34 +0300

    feat: database configuration

---
+++ app/src/main/java/com/ph03enixc0ders/rakibolanamalagasy/dao/tenyDAO.kt
@@ -0,0 +1,4 @@
+package com.ph03enixc0ders.rakibolanamalagasy.dao
+
+class tenyDAO {
+}
---
+++ app/src/main/java/com/ph03enixc0ders/rakibolanamalagasy/database/appDatabase.kt
@@ -0,0 +1,33 @@
+package com.ph03enixc0ders.rakibolanamalagasy.database
+
+import android.content.Context
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.ph03enixc0ders.rakibolanamalagasy.dao.tenyDAO
+
+
+abstract class appDatabase:RoomDatabase() {
+
+    companion object config{
+        val DATABASE_NAME="rakibolana_data.db"
+        var instance:appDatabase?=null
+        var LOCK= Object()
+
+        fun getInstance(context:Context):appDatabase{
+            if(instance==null){
+                synchronized(LOCK){
+                    if(instance==null){
+                        instance=Room.databaseBuilder(context.applicationContext,appDatabase::class.java,
+                        DATABASE_NAME).createFromAsset("rakibolana.db").build();
+
+                    }
+                }
+            }
+            return instance!!
+        }
+    }
+
+    abstract fun tenyDAO():tenyDAO;
+
+
+}
