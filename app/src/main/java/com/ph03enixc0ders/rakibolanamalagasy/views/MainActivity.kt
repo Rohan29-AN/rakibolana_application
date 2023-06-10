1d390e0519e98b7cd13f6877e096c830b60b64da
Tafita29 <rohanrc101@gmail.com>
2023-06-11 00:09:58 +0300

    feat: display the size of data in the db

--- app/src/main/java/com/ph03enixc0ders/rakibolanamalagasy/views/MainActivity.kt
+++ app/src/main/java/com/ph03enixc0ders/rakibolanamalagasy/views/MainActivity.kt
@@ -2,12 +2,17 @@ package com.ph03enixc0ders.rakibolanamalagasy.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.ph03enixc0ders.rakibolanamalagasy.R
 import com.ph03enixc0ders.rakibolanamalagasy.databinding.ActivityMainBinding
+import com.ph03enixc0ders.rakibolanamalagasy.viewmodels.tenyVM
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var _binding:ActivityMainBinding
+    lateinit var _viewModel:tenyVM
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,6 +34,14 @@ class MainActivity : AppCompatActivity() {
         //HIDE ACTION BAR
         this.supportActionBar?.hide();
 
-        //
+        //INITIALIZE VIEWMODEL
+        this._viewModel = ViewModelProvider(this, ViewModelProvider.AndroidViewModelFactory.getInstance(application))[tenyVM::class.java]
+
+
+        this._viewModel.getAllList().observe(this, Observer {
+            listTeny->
+                val _wordLength="Word length"+listTeny.size
+                _binding.wordNumber.setText(_wordLength)
+        })
     }
 }
