824e9055b5f9458bf6a5be2b29d9b6b6226f8dfc
Tafita29 <rohanrc101@gmail.com>
2023-06-11 16:18:00 +0300

    feat: using view binding on the fragment

--- app/src/main/java/com/ph03enixc0ders/rakibolanamalagasy/views/fragment/BookmarkFragment.kt
+++ app/src/main/java/com/ph03enixc0ders/rakibolanamalagasy/views/fragment/BookmarkFragment.kt
@@ -6,19 +6,29 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.ph03enixc0ders.rakibolanamalagasy.R
+import com.ph03enixc0ders.rakibolanamalagasy.databinding.FragmentHomeBinding
 
 class BookmarkFragment:Fragment() {
+    private var _binding: FragmentHomeBinding?=null
+    private  val binding get()=_binding!!
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_bookmark,container,false)
+        _binding=FragmentHomeBinding.inflate(inflater,container,false)
+        return  binding.root
     }
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding=null
     }
 }
--- app/src/main/java/com/ph03enixc0ders/rakibolanamalagasy/views/fragment/SettingFragment.kt
+++ app/src/main/java/com/ph03enixc0ders/rakibolanamalagasy/views/fragment/SettingFragment.kt
@@ -6,18 +6,30 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.ph03enixc0ders.rakibolanamalagasy.R
+import com.ph03enixc0ders.rakibolanamalagasy.databinding.FragmentHomeBinding
 
 class SettingFragment:Fragment() {
+
+    private var _binding: FragmentHomeBinding?=null
+    private  val binding get()=_binding!!
+
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_setting,container,false)
+        _binding=FragmentHomeBinding.inflate(inflater,container,false)
+        return  binding.root
     }
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding=null
     }
 }
