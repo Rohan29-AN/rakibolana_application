5df0b780ce747150a8e05c6383164c4b19f6b670
Tafita29 <rohanrc101@gmail.com>
2023-06-11 15:03:56 +0300

    feat: menu creation

---
+++ app/src/main/java/com/ph03enixc0ders/rakibolanamalagasy/views/Menu.kt
@@ -0,0 +1,12 @@
+package com.ph03enixc0ders.rakibolanamalagasy.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ph03enixc0ders.rakibolanamalagasy.R
+
+class Menu : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+    }
+}
