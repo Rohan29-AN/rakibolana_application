6a4607bc9b76563423d37f49585168db75a379df
Tafita29 <rohanrc101@gmail.com>
2023-06-15 11:07:37 +0300

    feat: handle item click event in ListView

--- app/src/main/java/com/ph03enixc0ders/rakibolanamalagasy/views/Historic.kt
+++ app/src/main/java/com/ph03enixc0ders/rakibolanamalagasy/views/Historic.kt
@@ -73,6 +73,7 @@ class Historic : AppCompatActivity(),OnClickItemInterface {
         this.adapter= historyAdapter(this, emptyList())
         this._binding.listView.adapter = this.adapter
         this.adapter.setOnItemCheckedChangeListener(this)
+        this.adapter.setOnItemClickedListener(this)
 
         //INITIALIZE VIEW MODEL
         this.viewModel = ViewModelProvider(this, ViewModelProvider.AndroidViewModelFactory.getInstance(application))[tenyVM::class.java]
@@ -95,7 +96,6 @@ class Historic : AppCompatActivity(),OnClickItemInterface {
         })
 
 
-
     }
 
     override fun onBackPressed() {
@@ -112,6 +112,10 @@ class Historic : AppCompatActivity(),OnClickItemInterface {
         else{
             this.listOfWordSelected.remove(item.id)
         }
+    }
+
+    override fun onItemClicked(item: teny) {
+
     }
 
 }
